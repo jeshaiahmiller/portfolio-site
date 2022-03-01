bb30d2165e713b8ea8194f31772f8088a85fae88
Joseph Miller <josephmiller@Josephs-MacBook-Pro.local>
2022-03-01 14:22:14 -0500

    updated resume link

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
       <Link className="link line" to="about">About Me</Link>
       <Link className="link line" to="projects">Projects</Link>
       <Link className="link line" to="contact">Contact</Link>
-      <a className="link" href="https://drive.google.com/file/d/1r6tGBxXMtbH3r14cN5NyfF2AGxVWZs_q/view" target="_blank" rel="noreferrer">Resume</a>
+      <a className="link" href="https://drive.google.com/file/d/1LGfvYFSGiMPDFet6wdv3AAzhnoQHeD79/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
   </div>
   )
 }
