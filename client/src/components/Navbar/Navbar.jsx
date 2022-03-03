9230f6ab01702589dc9cd8e23164714fbd189dc4
Joseph Miller <josephmiller@Josephs-MBP.attlocal.net>
2022-03-03 10:52:34 -0500

    updated resume link

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ import './Navbar.css'
 export default function Navbar() {
 
 
-  return(
+  return (
     <div className="navbar">
       <Link className="link line" to="/">Home</Link>
       <Link className="link line" to="about">About Me</Link>
       <Link className="link line" to="projects">Projects</Link>
       <Link className="link line" to="contact">Contact</Link>
       <a className="link" href="https://drive.google.com/file/d/1LGfvYFSGiMPDFet6wdv3AAzhnoQHeD79/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
-  </div>
+    </div>
   )
 }
